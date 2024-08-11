87857e9fe82470e70de60554f673abee40d2fdac
aris1377 <bek77519101@gmail.com>
2024-08-11 15:49:10 +0900

    feat: create routerAdmin  and member restaurantcontroller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin"
 /** 1-ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,5 +14,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use("/", router);
+app.use("/admin", routerAdmin); //SSR: EJS
+app.use("/", router);     // SPA: REACT
 export default app;
