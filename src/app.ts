720abb73f45a4335947cd59b608667c696a88d1f
aris1377 <bek77519101@gmail.com>
2024-08-11 17:48:41 +0900

    feat: create project standards

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import path from "path";
 import router from "./router";
-import routerAdmin from "./routerAdmin"
+import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
+
 /** 1-ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
 /** 2-SESSIONS **/
 
 /** 3-VIEWS **/
@@ -15,5 +19,5 @@ app.set("views engine", "ejs");
 
 /** 4-ROUTERS **/
 app.use("/admin", routerAdmin); //SSR: EJS
-app.use("/", router);     // SPA: REACT
+app.use("/", router); // SPA: REACT
 export default app;
