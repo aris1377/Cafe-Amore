b8316b142825a5832a4a5f239eb9d52c95e2df78
aris1377 <bek77519101@gmail.com>
2024-08-11 13:29:49 +0900

    feat: express integration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
+import app from "./app";
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connection secceed");
-      const PORT = process.env.PORT ?? 5500;
+    const PORT = process.env.PORT ?? 5500;
+    app.listen(PORT, function () {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((err) => console.log("Error on connection MongoDB", err));
