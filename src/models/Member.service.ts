9e9492c0048a75d0d001b58da3cf52cd157a21fb
aris1377 <bek77519101@gmail.com>
2024-08-13 22:15:19 +0900

    fix: modifiy restaurannt login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -3,12 +3,31 @@ import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { Message } from "../libs/Error";
 import { HttpCode } from "../libs/Error";
 import { MemberType } from "../libs/enums/member.enum";
-import e from "express";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
   constructor() {
     this.memberModel = MemberModel;
+  }
+
+  public async processSignup(input: MemberInput): Promise<Member> {
+    const exist = await this.memberModel
+      .findOne({ memberType: MemberType.RESTAURANT })
+      .exec();
+    console.log("exists", exist);
+    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+
+    try {
+      const result = await this.memberModel.create(input);
+      result.memberPassword = "";
+      return result;
+    } catch (err) {
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
   }
 
   public async processLogin(input: LoginInput): Promise<Member> {
@@ -22,28 +41,15 @@ class MemberService {
       .exec();
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMEBER_NICK);
 
-    const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
     if (!isMatch) {
       throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
     }
 
     return await this.memberModel.findById(member._id).exec();
-  }
-
-  public async processSignup(input: MemberInput): Promise<Member> {
-    const exist = await this.memberModel
-      .findOne({ memberType: MemberType.RESTAURANT })
-      .exec();
-    console.log("exists", exist);
-    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
-
-    try {
-      const result = await this.memberModel.create(input);
-      result.memberPassword = "";
-      return result;
-    } catch (err) {
-      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
-    }
   }
 }
 
