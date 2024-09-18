eddd9f74855aa6b4cf69e4c6bdb7ce414399fa3a
aris1377 <bek77519101@gmail.com>
2024-09-18 18:35:30 +0900

    feat: develop order controller, service, schema

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
 import productController from "./controllers/product.controller";
+import orderController from "./controllers/order.controller";
 
 /*Member*/
 router.get("/member/restaurant", memberController.getRestaurant);
@@ -36,5 +37,20 @@ router.get(
   productController.getProduct
 );
 
-
+/**Order */
+router.post(
+  "/order/create",
+  memberController.verifyAuth,
+  orderController.createOrder
+);
+router.get(
+  "/order/all",
+  memberController.verifyAuth,
+  orderController.getMyOrders
+);
+router.post(
+  "/order/update",
+  memberController.verifyAuth,
+  orderController.updateOrder
+);
 export default router;
