d472f37ca23e419d3230b70fa6bcab697cfa1c25
aris1377 <bek77519101@gmail.com>
2024-12-29 01:59:51 +0900

    feta: deploy

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,4 @@ git reset --hard
 git checkout master
 git pull origin master
 
-npm i
-npm run build 
-pm2 start process.config.js --env production
-
-#DEVELOPMENT
-# git reset --hard
-# git checkout develop
-# git pull origin develop
-
-# npm i
-# pm2 start "npm run start:dev" --name=CAFEAMORE
+docker compose up -d
