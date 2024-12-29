86e63c20be7ca21d6e04c9f7fabe2d69dc59cb85
aris1377 <bek77519101@gmail.com>
2024-12-29 18:39:01 +0900

    faet: 18:38

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-
 # PRODUCTION
 git reset --hard
 git checkout master
 git pull origin master
 
-npm i
-npm run build
-pm2 start process.config.js --env production
+docker compose up -d
